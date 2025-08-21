585b857aac1925b7c801c1d12b196f9d9d63c97f
pabitra <pabitram322@gmail.com>
2025-08-21 20:33:29 +0530

    feat: initialize backend with Express, MongoDB, and TypeScript setup
    - Added package.json with dependencies for Express, Mongoose, and JWT.
    - Created authController for handling login and signup requests.
    - Set up main server file (index.ts) with Express and MongoDB connection.
    - Defined user and expense schemas in model.ts.
    - Implemented basic auth routes in authRoutes.ts.
    - Configured TypeScript settings in tsconfig.json for the project.

---
+++ backend/src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import mongoose from "mongoose";
+config();
+
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!PORT) {
+  throw new Error("Port not defined");
+}
+
+if (!MONGO_URI) {
+  throw new Error("MongoDB URI not defined");
+}
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//routes
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log("Server running at http://localhost:" + PORT);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
---
+++ backend/src/models/model.ts
@@ -0,0 +1,40 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+const expenseSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: ["Food", "Transport", "Utilities", "Entertainment"],
+    required: true,
+  },
+});
